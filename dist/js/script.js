f4cc216d487f17777f4f2f6dbb87a9634c8a1f4a
unknown <popsgie>
2023-03-23 15:51:39 +0800

    header and banner for completion

--- dist/js/script.js
+++ dist/js/script.js
@@ -13,6 +13,7 @@ const headerDev = document.querySelector(".header__nav");
 toggleMenuDev.addEventListener("click", () => {
   toggleMenuDev.classList.toggle("open");
   headerDev.classList.toggle("open");
+  headerDev.style=("transition:.5s ease");
 });
 
 const toggleDotsDev = document.querySelector(".toggle__dots");
@@ -20,7 +21,29 @@ const headerDots = document.querySelector(".header__dots");
 toggleDotsDev.addEventListener("click", () => {
   toggleDotsDev.classList.toggle("open");
   headerDots.classList.toggle("open");
+  headerDots.style=("transition:.5s ease");
 });
 
+const hoverNav=document.querySelectorAll(".item");
+const hoverContent=document.querySelectorAll(".item__top");
+hoverNav.forEach((hover) => {
+    hover.addEventListener("click", () => {
+        removeActivehover();
+        hover.classList.add("active");
+        // const activeContent= document.querySelector(`#${hover.id}-content`);
+        // removeActiveContent();
+        // activeContent.classList.add("active");
+    })
+})
 
+function removeActiveStar(){
+  hoverNav.forEach((hover) =>{
+      hover.classList.remove("active");
+  })
+}
+// function removeActiveContent(){
+//   hoverContent.forEach((hover) =>{
+//       hover.classList.remove("active");
+//   })
+// }
 
